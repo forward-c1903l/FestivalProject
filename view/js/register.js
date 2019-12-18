3ad0b665315e21b454cdfdbdca628f580c5ae1e4
Đại <hathanhdai120491@gmail.com>
2019-12-18 20:10:49 +0700

    User template

---
+++ view/js/register.js
@@ -0,0 +1,9 @@
+document.getElementById("home-tab1").addEventListener("click", function(){
+    document.getElementById("myTabContentR").style.display = 'block';
+    document.getElementById("myTabContentL").style.display = 'none';
+})
+
+document.getElementById("home-tab2").addEventListener("click", function(){
+    document.getElementById("myTabContentL").style.display = 'block';
+    document.getElementById("myTabContentR").style.display = 'none';
+})
