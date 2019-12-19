968f38e667640b8c432f77c522f80803a451121b
Sang-GitHup020120 <sangolariel14@gmail.com>
2019-12-19 11:39:44 +0700

    $ about us map

--- view/js/about_us.js
+++ view/js/about_us.js
@@ -1,17 +1,2 @@
-// getLocation()
-// function getLocation() {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(showPosition);
-//     } else {
-//      console.log("Geolocation is not supported by this browser.");
-//     }
-// }
-// function showPosition(position) {
-//     var latlon = position.coords.latitude + "," + position.coords.longitude;
-  
-//     var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyBvC8Abuj9Y65fZNhtMbMhjhPVmXs74N_M";
-  
-//     document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
 
-//   }
 
