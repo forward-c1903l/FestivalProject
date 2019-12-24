de818a62c209e73a9f72753e02fb2e04584226d9
Sang-GitHup020120 <sangolariel14@gmail.com>
2019-12-24 11:24:31 +0700

    $ admin all religions page

--- view/js/admin.js
+++ view/js/admin.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
-    $('.toggle-click').click(function(){
+    $('.main-name-nav').click(function(){
         let dataId = parseInt($(this).attr("data-id"));
         $(this).toggleClass('nav-item-active');
         $(`.title-name-menu .wrap-title-nav:nth-child(${dataId}) .child-menu-nav`).slideToggle("slow");
     });
     $('.nav-toggle-click').click(function(){
         $(this).toggleClass('nav-active');
+        $(".container-menu-dashboard").toggleClass("container-nav-active");
+    });
+    $(`.edit-religions img:nth-child(1)`).click(function(){
+        $('.alert-delete-form').css({'opacity':'0.9',"transition":"0.8s","top": "50%"});
+    });
+    $(".wrap-btn button:nth-child(2)").click(function(){
+        $('.alert-delete-form').css({'opacity':'0',"top": "10%","transition":"0.5s"});
     })
 })
